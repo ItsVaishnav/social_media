f27a1b7573aa2303e5fe25c04262b023d727b4de
ItsVaishnav <vaishnavghadgepc99@gmail.com>
2025-08-17 12:15:05 +0530

    Header , footer , sideBar setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,16 @@
+import Header from "./Components/Header"
+import Footer from "./Components/Footer"
+import SideBar from "./Components/SideBar"
+import './App.css';
 function App() {
   return (
-    <>
-        
-    </>
+    <div className="app_container">
+        <SideBar></SideBar>
+        <div className="content">
+        <Header></Header>
+        <Footer></Footer>
+        </div>
+    </div>
   )
 }
 
